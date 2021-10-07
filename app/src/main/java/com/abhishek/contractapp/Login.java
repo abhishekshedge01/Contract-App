700a513d36977c7ca27b95e62d456d954189d788
Abhishek <abhishedge01@gmail.com>
2021-10-07 15:15:30 +0530

    Authentcation Code for Signup and Login.

--- app/src/main/java/com/abhishek/contractapp/Login.java
+++ app/src/main/java/com/abhishek/contractapp/Login.java
@@ -1,14 +1,95 @@
 package com.abhishek.contractapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
+    EditText email, password;
+    Button login_button;
+    ProgressDialog progressDialoglogin;
+    FirebaseAuth firebaseAuth;
+    TextView signup_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        signup_tv=findViewById(R.id.login_signup_textview);
+        email=findViewById(R.id.login_email);
+        password=findViewById(R.id.login_password);
+        firebaseAuth=FirebaseAuth.getInstance();
+        login_button=findViewById(R.id.login_login_button);
+        progressDialoglogin=new ProgressDialog(this);
+
+        signup_tv.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent=new Intent(getApplicationContext(),SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    private void Login()
+    {
+        String str1=email.getText().toString();
+        String str2=password.getText().toString();
+
+        if (TextUtils.isEmpty(str1)) {
+            email.setText("Mail not entered");
+            return;
+        }
+        if (TextUtils.isEmpty(str2)) {
+            password.setText("Password not entered");
+            return;
+        }
+
+        progressDialoglogin.setMessage("Processing...");
+        progressDialoglogin.setCanceledOnTouchOutside(false);
+        progressDialoglogin.show();
+
+        firebaseAuth.signInWithEmailAndPassword(str1,str2).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Intent intent=new Intent(getApplicationContext(),ContractList.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(Login.this,"Wrong Email or password entered",Toast.LENGTH_LONG).show();
+                }
+                progressDialoglogin.dismiss();
+            }
+        });
     }
 }
--- app/src/main/java/com/abhishek/contractapp/SignUp.java
+++ app/src/main/java/com/abhishek/contractapp/SignUp.java
@@ -1,14 +1,142 @@
 package com.abhishek.contractapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText fullname, email, password, confirm_password;
+    TextView login_tv;
+    Button signup_button;
+    ProgressDialog progressDialogsignup;
+    FirebaseAuth firebaseAuth;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
+
+        fullname = findViewById(R.id.signup_fullname);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+        confirm_password = findViewById(R.id.signup_confirm_password);
+        login_tv = findViewById(R.id.signup_login_textview);
+        signup_button = findViewById(R.id.signup_signup_button);
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialogsignup=new ProgressDialog(this);
+
+        signup_button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Register();
+            }
+        });
+
+        login_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void Register()
+    {
+        String string_fullname = fullname.getText().toString();
+        String string_email = email.getText().toString();
+        String string_password = password.getText().toString();
+        String string_confirmpassword = confirm_password.getText().toString();
+
+        if (TextUtils.isEmpty(string_fullname))
+        {
+            fullname.setError("enter your name");
+            return;
+        }
+
+        if (TextUtils.isEmpty(string_email))
+        {
+            email.setError("enter your mail");
+            return;
+        }
+
+        if (!checkUser(string_email)) {
+            email.setError("invalid email");
+            return;
+        }
+
+        if (TextUtils.isEmpty(string_password))
+        {
+            password.setError("enter your password");
+            return;
+        }
+
+        if (!TextUtils.isEmpty(string_password))
+        {
+            if (string_password.length() < 8) {
+                password.setError("password length <8");
+                return;
+            }
+        }
+
+        if (TextUtils.isEmpty(string_confirmpassword))
+        {
+            confirm_password.setError("enter your password");
+            return;
+        }
+
+        if(!string_password.equals(string_confirmpassword))
+        {
+            confirm_password.setError("password not similar to above field");
+            return;
+        }
+        progressDialogsignup.setMessage("Processing...");
+        progressDialogsignup.setCanceledOnTouchOutside(false);
+        progressDialogsignup.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(string_email,string_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
+                {
+                     Toast.makeText(getApplicationContext(),"Registration Successful",Toast.LENGTH_SHORT).show();
+                     Intent intent=new Intent(getApplicationContext(),ContractList.class);
+                     startActivity(intent);
+                     finish();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Account Already Exists!",Toast.LENGTH_SHORT).show();
+                }
+                progressDialogsignup.dismiss();
+            }
+        });
+    }
+
+    private boolean checkUser(String target)
+    {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 }
